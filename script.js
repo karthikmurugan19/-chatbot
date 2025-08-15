b40e4805e1a9722c2441ddf4778ce67d715c441e
karthikmurugan19 <134212331+karthikmurugan19@users.noreply.github.com>
2025-08-15 19:20:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -303,13 +303,6 @@ const handleOutgoingMessage = (e) => {
   generateBotResponse(incomingMessageDiv);
 };
 
-const picker = new EmojiMart.Picker({
-  theme: "light",
-  skinTonePosition: "none",
-  previewPostion: "none"
-});
-document.querySelector(".chat-form").appendChild(picker);
-
 // Submit + Enter to send
 document.querySelector(".chat-form").addEventListener("submit", handleOutgoingMessage);
 messageInput.addEventListener("keydown", (e) => {
